be703515aebfd3c6ac437d94a729b1380c9c9e04
Ivan Suiyama Silva <ivan.suiya@gmail.com>
2025-12-12 16:29:07 -0300

    Feat: adicionando trabalhos mais recentes

--- app.py
+++ app.py
@@ -10,9 +10,3 @@ def index():
 if __name__ == '__main__':
     app.run()
 
-
-'''
-Colocar uma foto mais profissional, adicionar
-mecanismo de enviar email, adcionar listas de type e Java,
-listas de javascript, projetinhos em Python, Arrumar seção de sobre. Colocar mais animações,
-quando clicar na foto, expandir o tamanho (nos projetos), adicionar aplicativo'''
